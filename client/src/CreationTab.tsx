755a548d47ab9ae9c5d85b301ec1edea15a5d11c
Kevin Delgado Ojeda <clinicdev67@gmail.com>
2026-05-28 00:46:53 -0400

    Finished Details panel, marking this the completion of the Creation Tab. Damn, this frontend stuff stucks.

--- client/src/CreationTab.tsx
+++ client/src/CreationTab.tsx
@@ -1,11 +1,17 @@
 import "./CreationTab.css";
 import icon from "./assets/3d-glasses.png";
 import fileDrop from "./assets/fileupload.png";
+import checkImage from "./assets/check.png";
 import { useState, type ChangeEvent } from "react";
 
 function CreationTab() {
   const [image, setImage] = useState<string>("");
   const [bool, setBool] = useState<boolean>(false);
+  const [optionValue, setOptionValue] = useState<string>("");
+  const [movieTitle, setMovieTitle] = useState<string>("");
+  const [movieLength, setMovieLength] = useState<string>("");
+  const [rating, setRating] = useState<string>("");
+  const [date, setDate] = useState<string>("");
 
   function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
     const file = e.target.files?.[0];
@@ -27,6 +33,10 @@ function CreationTab() {
       setImage(imageUrl);
       setBool(true);
     }
+  }
+
+  function getOptionValue(e: ChangeEvent<HTMLSelectElement>) {
+    setOptionValue(e.target.value);
   }
 
   return (
@@ -82,8 +92,57 @@ function CreationTab() {
           </div>
         </div>
         <div className="right-side">
-          <input placeholder="Movie Title" />
+          <p className="title5">Details</p>
+          <input
+            onChange={(e) => {
+              setMovieTitle(e.target.value);
+            }}
+            className="general-input"
+            placeholder="Movie Title"
+          />
+          <input
+            onChange={(e) => {
+              setMovieLength(e.target.value);
+            }}
+            className="general-input"
+            placeholder="Movie Length"
+          />
+          <select className="select-element" onChange={getOptionValue}>
+            <option value="">Watched Previously?</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+          {optionValue.toLowerCase() === "yes" && (
+            <>
+              <div className="rating-container">
+                <input
+                  onChange={(e) => {
+                    setRating(e.target.value);
+                  }}
+                  className="general-input"
+                  placeholder="Rating"
+                />
+                <p className="title7">/10</p>
+              </div>
+              <div className="date-container">
+                <p className="title6">Date Watched: </p>
+                <input
+                  onChange={(e) => {
+                    setDate(e.target.value);
+                  }}
+                  className="general-input2"
+                  type="date"
+                />
+              </div>
+            </>
+          )}
         </div>
+      </div>
+
+      <div className="check-button-container">
+        <button className="check-button">
+          <img className="check-img" src={checkImage} />
+        </button>
       </div>
     </div>
   );
